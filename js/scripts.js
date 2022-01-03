c8e7b7a561b108e38bf666d45b2afc9dc4a43b06
tlarson1010 <tristynlarson@gmail.com>
2022-01-02 18:58:13 -0700

    Added Scoreboard Modal

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+
+// index.html scripts
+$(function() {
+    $(document).ready(function(){
+        $("#scoreboardButton").click(function(){
+            $("#scoreboardModal").modal("toggle");
+        });
+        console.log(localStorage.getItem("totalTime"));
+        
+        // $("#loginButton").click(function(){
+        //     $("#loginModal").modal("toggle");
+        // });
+    });
+
+    $("#timeButton").click(function(){
+        if($("#timeLabel").hasClass("active")){
+            console.log("timed game");
+            $("#totalTimeDiv").removeClass("d-none");
+            $("#targetScoreDiv").addClass("d-none");
+        }
+    });
+
+    $("#scoreButton").click(function(){
+        if($("#scoreLabel").hasClass("active")){
+            console.log("score game");
+            $("#totalTimeDiv").addClass("d-none");
+            $("#targetScoreDiv").removeClass("d-none");
+        }
+    });
+
+    $("#numPlayers").change(function(){
+        localStorage.setItem("numPlayers", $("#numPlayers").val());
+    });
+
+    $("#totalTime").change(function(){
+        localStorage.setItem("totalTime", $("#totalTime").val());
+    });
+
+    $("#targetScore").change(function(){
+        localStorage.setItem("targetScore", $("#targetScore").val());
+    });
+
+});
+
+function showInfo() {
+    
+}
